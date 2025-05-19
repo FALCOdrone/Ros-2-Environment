cd2af7acb3cd8314fa5503c96e16230b4e686f7d
Gabriel Voss <voss.git@gmail.com>
2025-05-19 15:38:38 +0200

    .

--- bumperbot_ws/build/urdf_tutorial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /github/ros2odometry/bumperbot_ws/src/urdf_tutorial
-# Build directory: /github/ros2odometry/bumperbot_ws/build/urdf_tutorial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
