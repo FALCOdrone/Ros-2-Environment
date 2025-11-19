14f28f3cb5766c1a7da6c57d3c2a19933970df4b
Relo02 <lorenzo1.ortolani@mail.polimi.it>
2025-11-19 23:22:53 +0100

    new package setting

--- drone_control_ws/src/drone_control/setup.py
+++ drone_control_ws/src/drone_control/setup.py
@@ -10,22 +10,21 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/px4_gazebo_iris.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Lorenzo',
-    maintainer_email='lorenzo@example.com',
-    description='ROS2 control system for quadrotor drone',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer='lorenzo',
+    maintainer_email='ortolore@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
-            'drone_controller = drone_control.drone_controller:main',
-            'state_estimator = drone_control.state_estimator:main',
-            'trajectory_planner = drone_control.trajectory_planner:main',
-            'px4_bridge = drone_control.px4_bridge:main',
-            'mission_commander = drone_control.mission_commander:main',
+            'motors_pub = drone_control.scripts.motors_pub:main'
         ],
     },
 )
